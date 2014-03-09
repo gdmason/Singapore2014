d8d4ea3fd34779abe5160aedb22a0fdcc821586a
Gabriel <gabrielmason@mac.com>
2014-03-09 16:42:44 +0800

    Updated Navbar with glyphicons and updated home route

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Pinteresting::Application.routes.draw do
   resources :pins
 
   devise_for :users
-  root "pages#home"
+  root "pins#index"
   get "about" => "pages#about" #creates about path
   get "carouseltest" => "pages#carouseltest" #creates carouseltest path
     # The priority is based upon order of creation: first created -> highest priority.
